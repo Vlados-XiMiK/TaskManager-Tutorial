a3311eeee5a257d9a16773728a4b94a4235154b4
unknown <tvladislav0504@gmail.com>
2024-11-07 16:58:37 +0200

    105 connect to db

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,2 +1,12 @@
-const connections =
-  'mongodb+srv://john:<db_password>@nodeexpressproject.exzkp.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProject'
+const mongoose = require('mongoose')
+
+const connectionString =
+  'mongodb+srv://vlad:1234@nodeexpressproject.exzkp.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProject'
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('CONNECTED TO THE DB...'))
+  .catch((err) => console.log(err))
