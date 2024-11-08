7e33b659914597d3d9e5dace7fdae40c77ea096b
unknown <tvladislav0504@gmail.com>
2024-11-08 10:30:25 +0200

    106 refactor connect

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose')
 const connectionString =
   'mongodb+srv://vlad:1234@nodeexpressproject.exzkp.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProject'
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err))
+const connectDB = (url) => {
+  return mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('CONNECTED TO THE DB...'))
+    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
