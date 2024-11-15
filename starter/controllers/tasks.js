1068c58fdcee5f59e545cae147e506aeab51ef7b
unknown <tvladislav0504@gmail.com>
2024-11-15 11:12:53 +0200

    115 deleteTask controller

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -24,7 +24,7 @@ const getTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with id: ${taskID}` })
     }
-    res.status(200).json({ task })
+    //
   } catch (error) {
     res.status(500).json({ msg: error })
   }
@@ -32,8 +32,17 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send('update task')
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = {
